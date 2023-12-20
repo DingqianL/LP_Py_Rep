5a9f2a6685c51b83ab30216a9b865dce65c56a56
Dingqian Sara Liu <dl5165a@american.edu>
2023-12-20 07:47:13 -0500

    Update and rename replicate Jorda stata simple example_lp_example.py to lp_example.py
    write more comments on the code

--- lp_example.py
+++ lp_example.py
@@ -1,12 +1,18 @@
-# import packages
+# 
+# Author: Dingqian Liu, email: saradingqian.liu@gmail.com.
+# Comments & suggestions are welcome.
+# This file is a mirror replication of lp_example.do at Òscar Jordà's website: https://sites.google.com/site/oscarjorda/home/local-projections
+# Efforts are made to harmonize the coding language differences between Python and Stata. Please feel free to let me know if you find errors.
+# Disclaimer: the material on this website does not represent the views of American Century Investments.
+
+#import packages
 import pandas as pd
-import statsmodels.stats.sandwich_covariance as newey
 import numpy as np
 import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
 
 
-df = pd.read_stata(r'AED_INTERESTRATES.DTA')
+df = pd.read_stata(r'AED_INTERESTRATES.DTA') # please download the DTA file as describled in lp_example.do. For folks who are not familiar with Stata, *.do file can be opened with Notepad text editor.
 
 # choose impulse response horizon
 hmax = 12
@@ -17,7 +23,7 @@ df['daten'] = pd.to_datetime(df['daten'])
 # Step 1: Generate LHS variables for the LPs 
 
 for h in range(hmax+1):
-    df[f'gs10_{h}'] = df['gs10'].shift(-h)  # levels, in stata, shift(-1) is equiavalent to f1.var
+    df[f'gs10_{h}'] = df['gs10'].shift(-h)  # levels, shift(-1) is equiavalent to f1.var in stata
     df[f'gs10d{h}'] = df['gs10'].diff(1).shift(-h)   # differences
     df[f'gs10c{h}'] = df['gs10'].shift(-h) - df['gs10'].shift(1)  # Cumulative
 
@@ -35,15 +41,16 @@ for i in range(1, 3+1):
 b = []
 u = []
 d = []
+
 for h in range(hmax+1):
-    reg = smf.ols(f'gs10_{h} ~ gs1_0 + gs1_1 + gs1_2 + gs1_3 + gs1_4 + gs10_l1 + gs10_l2 + gs10_l3', data = df).fit(cov_type='HAC', cov_kwds={'maxlags':h})
+    reg = smf.ols(f'gs10_{h} ~ gs1_0 + gs1_1 + gs1_2 + gs1_3 + gs1_4 + gs10_l1 + gs10_l2 + gs10_l3', data = df).fit(cov_type='HAC', cov_kwds={'maxlags':h}) # you may find alternative way to specify the functions
     b.append(reg.params['gs1_0'])
     u.append(reg.params['gs1_0'] + 1.645*reg.bse['gs1_0'])
     d.append(reg.params['gs1_0'] - 1.645*reg.bse['gs1_0'])
 
 df_result = pd.DataFrame(data = {'Upper bund': u, 'Lower bund': d, 'IR': b})
 
-# Impulse Response Funtion graph
+# Impulse Response Funtion graph; can be improved to produce prettier graph
 ax = df_result.plot(title='Impulse response of GS10 to 1pp shock to GS1', ylim = (0, 1.5))
 ax.set_xlabel("Year")
 ax.set_ylabel("Percent")
